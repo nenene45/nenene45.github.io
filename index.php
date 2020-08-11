cf435953c94834da04c1af49ed35eba99fe9c8ed
nenene45 <50399551+nenene45@users.noreply.github.com>
2020-08-11 11:38:35 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="refresh" content="3000">
+</head>
+
+<body>
+	<br>
+	<table width=100% height=800>
+		<tr height=800>
+			<td width=50% height=100%>
+				<p><b>http://223.171.41.106:3080/</b></p>
+				<iframe src="http://223.171.41.106:3080/" frameborder="0" height=100% width=100%></iframe>
+			</td>
+			<td width=50% height=100%>
+				<p><b>http://223.171.41.98:3080/</b></p>
+				<iframe src="http://223.171.41.98:3080/" frameborder="0" height=100% width=100%></iframe>
+			</td>
+		</tr>
+	</table>
+
+</body>
+
+</html>
